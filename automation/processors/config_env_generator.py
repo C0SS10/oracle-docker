2575236f50f3ceed46d5f703d85034a0cd1f7fac
Esteban <estebancg12@hotmail.com>
2025-12-11 13:22:16 +0000

    [Fix] update config.env generation logic and content

--- automation/processors/config_env_generator.py
+++ automation/processors/config_env_generator.py
@@ -3,26 +3,26 @@ from pathlib import Path
 class EnvGenerator:
     @staticmethod
     def create(prefix: str, date: str, dump_files: list[str], project_root: Path):
-        scienti_path = project_root / "scienti"
-        scienti_path.mkdir(exist_ok=True)
+        dump_files_joined = ",".join(dump_files)
+        home_name = Path.home().as_posix()
+        dump_abs = f"{home_name}/dump"
 
-        env_file = scienti_path / ".config.env"
-
-        dump_joined = ",".join(dump_files)
-
-        content = f"""#!/bin/bash
+        env_content = f"""#!/bin/bash
 
 export CVLAC_USER="{prefix}_CV"
 export GRUPLAC_USER="{prefix}_GR"
 export INSTITULAC_USER="{prefix}_IN"
-export ORACLE_PWD="password"
+export ORACLE_PWD="colavudea"
 
-export DUMP_PATH="$HOME/dump"
+export DUMP_PATH="{dump_abs}"
 export DUMP_DATE="{date}"
-export DUMP_FILES="{dump_joined}"
+export DUMP_FILES="{dump_files_joined}"
 
 export HUNABKU_PORT=9090
 """
 
-        env_file.write_text(content)
-        return env_file
+        env_path = project_root / "scienti" / "config.env"
+        env_path.write_text(env_content)
+
+        print(f"📝 Archivo config.env generado en: {env_path}")
+        return env_path
