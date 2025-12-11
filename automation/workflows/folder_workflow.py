9e61a37dfa7fe7e2a5dc5383a7d2b5fb2e420e24
Esteban <estebancg12@hotmail.com>
2025-12-11 13:22:23 +0000

    [Add] enhance FolderWorkflow to include project_root and execute Docker commands

--- automation/workflows/folder_workflow.py
+++ automation/workflows/folder_workflow.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+from commands.docker_down_command import DockerDownCommand
+from commands.docker_up_command import DockerUpCommand
+from commands.kaypacha_command import SqlToMongoCommand
+from orchestrator.command_executor import CommandExecutor
 from drive.drive_service import DriveService
 from processors.config_env_generator import EnvGenerator
 from processors.dump_metadata import DumpMetadata
@@ -7,10 +11,10 @@ from processors.zip_processor import ZipProcessor
 
 
 class FolderWorkflow:
-
-    def __init__(self, drive: DriveService, base_dump: Path):
+    def __init__(self, drive: DriveService, base_dump: Path, project_root: Path):
         self.drive = drive
         self.base_dump = base_dump
+        self.project_root = project_root
 
     def process_folder(self, folder):
         folder_name = folder["name"]
@@ -52,5 +56,15 @@ class FolderWorkflow:
             project_root=Path(__file__).resolve().parents[2],
         )
 
-        print(f"📝 Config generado: {env_file}")
+        if env_file:
+            executor = CommandExecutor()
+
+            compose_file = self.project_root / "scienti" / "docker-compose.yml"
+
+            executor.add(DockerUpCommand(compose_file=compose_file, env_file=env_file))
+            executor.add(SqlToMongoCommand(prefix=prefix, dump_date=date))
+            executor.add(DockerDownCommand(compose_file=compose_file, env_file=env_file))
+
+            executor.run()
+
         return env_file
